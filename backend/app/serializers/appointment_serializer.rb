ac889f8f588b7ca73ac9b155f7e9bee27478eb6e
umberto palazzo <upalazzo93@gmail.com>
2020-06-14 21:12:54 -0400

    created serializers for models

---
+++ backend/app/serializers/appointment_serializer.rb
@@ -0,0 +1,4 @@
+class AppointmentSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes 
+end
---
+++ backend/app/serializers/establishment_serializer.rb
@@ -0,0 +1,4 @@
+class EstablishmentSerializer
+  include FastJsonapi::ObjectSerializer
+  attributes 
+end
