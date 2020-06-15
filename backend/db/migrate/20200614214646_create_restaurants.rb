232cf1c02e83c1320dd0ad5a6adf84b37261dab9
umberto palazzo <upalazzo93@gmail.com>
2020-06-14 20:09:15 -0400

    added and migrated user

--- backend/db/migrate/20200614214646_create_restaurants.rb
+++
@@ -1,8 +0,0 @@
-class CreateRestaurants < ActiveRecord::Migration[5.2]
-  def change
-    create_table :restaurants do |t|
-
-      t.timestamps
-    end
-  end
-end
--- backend/db/migrate/20200615000838_create_users.rb
+++ backend/db/migrate/20200615000838_create_users.rb
@@ -1,6 +1,9 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.string :username
+      t.string :gender
+      t.string :interests
 
       t.timestamps
     end
