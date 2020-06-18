fb4e94e47ccd6c45a8d452c6bcac8081359296ef
umberto palazzo <upalazzo93@gmail.com>
2020-06-18 00:06:48 -0400

    added thunk and refactored to work

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,15 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // import * as serviceWorker from './serviceWorker';
-import manageUser from './reducers/manageUser'
+import manageUsers from './reducers/manageUsers'
 
 import { Provider } from 'react-redux'
-import { createStore, compose, applyMiddleware } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
-const store = createStore(manageUser, composeEnhancer(applyMiddleware(thunk)));
+const store = createStore(manageUsers, applyMiddleware(thunk));
 
 
 // ReactDOM.render(
@@ -26,7 +24,7 @@ ReactDOM.render(
     <App />
   </Provider>,
   document.getElementById('root')
-)
+);
 
 
 // If you want your app to work offline and load faster, you can change
