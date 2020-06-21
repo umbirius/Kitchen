b18da86227182012b58f369a79c7a2b218e8315f
umberto palazzo <upalazzo93@gmail.com>
2020-06-21 17:41:28 -0400

    current user updates with ADD_USER

--- frontend/src/reducers/manageUsers.js
+++ frontend/src/reducers/manageUsers.js
@@ -16,7 +16,7 @@ export default function manageUsers(state = {
         
         case 'ADD_USER':
             const user = { username: action.user.text, gender: action.user.gender, interests: action.user.text, id: cuid() }
-            return { ...state, users: [...state.users, user] }
+            return { ...state, users: [...state.users, user], current_user: user }
 
         case 'DELETE_USER':
             return { ...state, users: state.users.filter(user => user.id !== action.id) }
