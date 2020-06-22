7b06e38b4c2f121396ec053a565a16e4c3847be8
umberto palazzo <upalazzo93@gmail.com>
2020-06-21 23:37:01 -0400

    added faker and new seed file

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -7,6 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 require 'csv' 
+require 'faker'
 
 csv_text = File.read(Rails.root.join('lib/user_seed.csv'))
 csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
