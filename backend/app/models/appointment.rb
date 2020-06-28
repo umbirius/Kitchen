455d4b5da6dc5fcb2dc13196f0609687e8ac4d6f
umberto palazzo <upalazzo93@gmail.com>
2020-06-28 14:56:09 -0400

    user association updated

--- backend/app/models/appointment.rb
+++ backend/app/models/appointment.rb
@@ -1,4 +1,7 @@
 class Appointment < ApplicationRecord
     belongs_to :establishment
-    belongs_to :user, optional: true
+
+    belongs_to :first_user,  foreign_key: :user_id_one, class_name: "User", optional: true
+    belongs_to :second_user,  foreign_key: :user_id_two, class_name: "User", optional: true
+
 end
