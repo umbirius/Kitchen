a1e410274020f7def9050ac52298945480589209
umberto palazzo <upalazzo93@gmail.com>
2020-06-28 14:56:35 -0400

    updated user assoication

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
-    has_many :appointments
+
+    has_many :appointments, ->(user){ unscope(where: :user_id).where "user_id_one = ? OR user_id_two = ?", user.id, user.id }
+    
 end
