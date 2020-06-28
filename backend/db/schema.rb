5b8d1e6f97011493f6a15ed941d4a128c9f34a75
umberto palazzo <upalazzo93@gmail.com>
2020-06-28 15:10:47 -0400

    updates appointments table

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_16_003330) do
+ActiveRecord::Schema.define(version: 2020_06_28_190807) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,12 +18,12 @@ ActiveRecord::Schema.define(version: 2020_06_16_003330) do
   create_table "appointments", force: :cascade do |t|
     t.datetime "when"
     t.integer "establishment_id"
-    t.string "type"
     t.integer "user_id_one"
     t.integer "user_id_two"
     t.string "code_word"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "status", default: false
   end
 
   create_table "establishments", force: :cascade do |t|
