5d9cceffc96fcc85535b2f95aaf2789b04a2f8d1
umberto palazzo <upalazzo93@gmail.com>
2020-06-29 23:22:47 -0400

    date request posts

--- backend/app/controllers/appointments_controller.rb
+++ backend/app/controllers/appointments_controller.rb
@@ -1,6 +1,21 @@
+
 class AppointmentsController < ApplicationController
     def index
         appointments = Appointment.all
         render json: AppointmentSerializer.new(appointments)
-      end
+    end
+
+    def update
+        appointment = Appointment.find_by(id: params[:id])
+        if !appointment.user_id_one 
+            byebug
+            appointment.update(user_id_one: params[:user_id])
+        elsif appointment.user_id_one && !appointment.status
+            byebug
+            appointment.update(user_id_two: params[:user_id], status: true)
+        end 
+        render json: AppointmentSerializer.new(appointment)
+    end
+
+
 end
