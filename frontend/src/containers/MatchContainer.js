79c33161fad4e587cc94149e7f47acda58011ffb
umberto palazzo <upalazzo93@gmail.com>
2020-07-08 21:49:12 -0400

    matches sort by date

--- frontend/src/containers/MatchContainer.js
+++ frontend/src/containers/MatchContainer.js
@@ -7,7 +7,7 @@ class MatchContainer extends Component {
 
     render() {
         const matches = this.props.appointments.filter(apt => (apt.user_id_one == this.props.current_user.id || apt.user_id_two == this.props.current_user.id)
-            && apt.status == true)
+            && apt.status == true).sort(dateSort)
 
         return (
             <div className="center-app">
@@ -27,3 +27,18 @@ const mapStateToProps = (state) => ({
 })
 
 export default connect(mapStateToProps)(MatchContainer)
+
+function dateSort(a, b) {
+
+    const dateA = a.when;
+    const dateB = b.when;
+
+    let comparison = 0;
+    if (dateA > dateB) {
+        comparison = 1;
+    } else if (dateA < dateB) {
+        comparison = -1;
+    }
+    return comparison;
+
+}
